1757e7a785de177f934ed84cb02e8f25c3b4e8e6
Lucas Medeiros <lucastoc@gmail.com>
2016-04-19 08:36:21 -0300

    add documentation for the configure method

--- lib/rslack/configuration.rb
+++ lib/rslack/configuration.rb
@@ -5,6 +5,19 @@ class RSlack::Configuration
 
   attr_accessor :token
 
+  # Public: Create a configuration instance with a configuration block
+  # and stores itself in its own class
+  #
+  # block - A required block that must set the token field with the token
+  #         registered for your bot in your slack team bot section.
+  #
+  # Examples
+  #
+  #   RSlack::Configuration.configure do |config|
+  #     config.token = ENV['SLACK_BOT_TOKEN']
+  #   end
+  #
+  # Returns the configuration that was created
   def self.configure
     raise 'A configuration block must be passed' unless block_given?
     @current = self.new

85e909acc6539b530cddafa8537abb6e1c1150fc
Lucas Medeiros <lucastoc@gmail.com>
2016-04-19 16:08:07 -0300

    add start method documentation

--- lib/rslack/rtm/api.rb
+++ lib/rslack/rtm/api.rb
@@ -1,9 +1,32 @@
 require 'rest-client'
 
+# Public: Various RTM API methods.
+#
+# Examples
+#
+#   class Client
+#     include RSlack::RTM::API
+#   end
 module RSlack
   module RTM
     module API
 
+      # Public: Abstraction for the Slack RTM API start method call
+      #         (https://api.slack.com/methods/rtm.start)
+      #
+      # Examples
+      #
+      #   => client.start
+      #
+      # Returns a hash with the options explained on the api method call
+      # definition on the website or the following errors:
+      #
+      #   ConnectionFailedError => HTTP Errors
+      #   MigrationInProgressError => Error on Slack's RTM API: migration_in_progress
+      #   NotAuthenticatedError => Error on Slack's RTM API: not_authed
+      #   InvalidAuthError => Error on Slack's RTM API: invalid_auth
+      #   AccountInactiveError => Error on Slack's RTM API: account_inactive
+      #   InvalidCharsetError => Error on Slack's RTM API: invalid_charset
       def start
         config = RSlack::Configuration.current
         url = "#{config.api_url}/rtm.start?token=#{config.token}"
